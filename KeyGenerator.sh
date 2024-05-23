2bb48f7c394585b38a192c2a9b95497481c60fc3
Meirelez <131269269+Meirelez@users.noreply.github.com>
2024-05-23 15:48:45 +0100

    Bash script that allows us to generate ZSKs
    This script allows us to generate ZSKs and collect the ones that have KeyTag that we specify.

---
+++ KeyGenerator.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+KEY_ZONE=example.edu
+DESIRED_KEYTAG=34345
+
+while true; do
+
+    for i in {1..200}; do
+
+        #KEYTAG=$(dnssec-keygen -a RSASHA256 -b 2048 -f KSK ${KEY_ZONE} | cut -f3 -d'+')
+        KEYTAG=$(dnssec-keygen -a ECDSAP256SHA256 -b 1024  ${KEY_ZONE} | cut -f3 -d'+')
+        if [[ $KEYTAG = *${DESIRED_KEYTAG}* ]]; then
+            echo "Key with desired tag has been generated."
+            exit
+        fi
+
+        #Keep a log of which key tags we've seen
+        echo $KEYTAG >> keytags.txt
+
+    done
+
+    #Clean up and relax
+    rm -f *.key *.private
+    sleep 7
+
+done
